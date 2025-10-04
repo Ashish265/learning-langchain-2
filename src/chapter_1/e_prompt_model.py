fc187085987f5863d9996b767ae97ed897782128
Ashish265 <ashishgupta2692@gmail.com>
2025-10-04 09:09:36 +0000

    chat model prompt

---
+++ src/chapter_1/e_prompt_model.py
@@ -0,0 +1,33 @@
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+load_dotenv()
+
+template = PromptTemplate(template="""
+                          Answer the question based on the context below.
+                          If the question cannot be answered
+                          based on the context,say "I don't know".
+                          Context : {context}
+                          Question : {question}
+                          Answer: """)
+model = ChatOpenAI()
+
+prompt = template.invoke(
+    {
+        "context": """
+        The most recent advancements in NLP
+        are being driven by Large Language Models (LLMs).
+        These models outperform their smaller counterpart and
+        have become invaluable for developers
+        who are creating applications with NLP capabilities.
+        Developers can tap into these models
+        through Hugging Face's `transformers` library,
+        or by utilizing OpenAI and Cohere's offerings
+        through the `openai` and `cohere` libraries, respectively.
+        """,
+        "question": "Which model providers offer LLMs?",
+    }
+)
+
+response = model.invoke(prompt)
+print(response.content)
