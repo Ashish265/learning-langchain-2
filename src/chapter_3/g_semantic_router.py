9e4315922e7ba7332323227b20cc51c03238c3c7
Ashish265 <ashishgupta2692@gmail.com>
2025-10-12 15:15:42 +0000

    sql self query

---
+++ src/chapter_3/g_semantic_router.py
@@ -0,0 +1,47 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import chain
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
+
+load_dotenv()
+
+physics_template = """You are a very smart physics professor.
+You are great at answering questions about physics
+in a concise and easy-to-understand manner.
+When you don't know the answer to a question,
+you admit that you don't know. Here is a question: {query}"""
+
+math_template = """You are a very good mathematician.
+You are great at answering math questions.
+You are so good because you are able to break down hard
+problems into their component parts,
+answer the component parts,
+and then put them together to
+answer the broader question. Here is a question: {query}"""
+
+embeddings = OpenAIEmbeddings()
+prompt_templates = [physics_template, math_template]
+prompt_embeddings = embeddings.embed_documents(prompt_templates)
+
+
+@chain
+def prompt_router(input: dict):
+    query = input["query"]
+    query_embedding = embeddings.embed_query(query)
+    similarity = cosine_similarity([query_embedding], prompt_embeddings)[0]
+    most_similar = prompt_templates[similarity.argmax()]
+    print("Using MATH" if most_similar == math_template else "Using PHYSICS")
+    return PromptTemplate.from_template(
+        most_similar
+        ).format_prompt(query=query).to_string()
+
+
+llm = ChatOpenAI()
+output_parser = StrOutputParser()
+
+semantic_router = prompt_router | llm | output_parser
+
+result = semantic_router.invoke({"query": "What's a black hole"})
+print("\nSemantic router result: ", result)
