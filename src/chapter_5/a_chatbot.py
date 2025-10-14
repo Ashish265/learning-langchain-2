d24c5bba5e2499537418295b8efbcee8630b4306
Ashish265 <ashishgupta2692@gmail.com>
2025-10-14 17:49:00 +0000

    chatbot

---
+++ src/chapter_5/a_chatbot.py
@@ -0,0 +1,39 @@
+from typing import Annotated, TypedDict
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages`
+    # function in the annotation defines how this state should
+    # be updated (in this case, it appends new messages to the
+    # list, rather than replacing the previous messages)
+    messages: Annotated[list, add_messages]
+
+
+def chatbot(state: State):
+    answer = model.invoke(state["messages"])
+    return {"messages": [answer]}
+
+
+builder = StateGraph(State)
+
+builder.add_node("chatbot", chatbot)
+
+builder.add_edge(START, "chatbot")
+builder.add_edge("chatbot", END)
+
+graph = builder.compile()
+
+# Example usage
+
+input = {"messages": [HumanMessage("hi!")]}
+for chunk in graph.stream(input):
+    print(chunk)
